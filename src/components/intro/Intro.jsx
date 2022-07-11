04fe454a83e3e4637882f6b32fe93c618c6604ad
arunaganeshraj <aruna.durai@gmail.com>
2022-07-11 23:47:09 +0100

    Intro page changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,27 @@
 import './Intro.scss';
-
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 const Intro = () => {
   return (
-    <div id='intro' className='intro'>Intro</div>
+    <div id='intro' className='intro'>
+      <div className="left">
+        <div className="imageContainer">
+         <img src="assets/aruna.jpg" alt="profile" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>
+           Hi There, I'm
+          </h2>
+          <h1>Aruna Duraisamy</h1>
+          <h3>Web Developer</h3>
+          <a href="#about">
+          <KeyboardDoubleArrowDownIcon className='icon' />
+          </a>
+        </div>
+       
+      </div>
+    </div>
   )
 }
 
